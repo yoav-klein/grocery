05cd3a172d68031241c30990d94349f8c877e50b
Yoav <yoavklein25@gmail.com>
2024-06-22 23:07:23 +0300

    Starter

---
+++ app/src/main/java/com/grocery/MyController.java
@@ -0,0 +1,17 @@
+package com.grocery;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    
+    @RequestMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello, Spring MVC! This is using AbstractDispatcherServletInitializer.");
+        return "/WEB-INF/hellopage.jsp"; // This corresponds to the view name
+    }    
+    
+}
