23caf5de4b9e6d8e06b0824bbbde8497b61045d7
Yoav <yoavklein25@gmail.com>
2025-12-19 02:48:43 +0200

    UI tests

---
+++ ui/layout/main.js
@@ -0,0 +1,18 @@
+
+const overlayEl = document.getElementById('overlay');
+
+const toggleSidebarButtonEl = document.getElementById("menu-button");
+toggleSidebarButtonEl.addEventListener('click', toggleSidebar);
+const sidebarEl = document.getElementById("sidebar");
+
+
+function toggleSidebar() {
+    
+    const isMobile = window.innerWidth <= 768;
+    sidebarEl.classList.toggle('close');
+    
+    if (isMobile) {
+        overlayEl.classList.toggle('active');
+    }
+}
+
